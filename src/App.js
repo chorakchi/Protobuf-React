e2c0070cae3251aee6d3c52b6d9ddac45b199fcc
mohamad chorakchi <m.chorakchi@ymail.com>
2018-05-10 22:29:05 +0430

    Install and import protobufjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import protobuf from 'protobufjs'
 import './App.css';
 
 class App extends Component {
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+           Get start with Protobufjs in React.
         </p>
       </div>
     );
