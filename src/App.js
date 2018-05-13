0fc7a83d7060d1d0abfbc5822a0d082b38515cde
mohamad chorakchi <m.chorakchi@ymail.com>
2018-05-13 22:32:14 +0430

    using of protobuf

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
-import protobuf from 'protobufjs'
+import { load } from "protobufjs";
 import './App.css';
+import awesomProto from './awesome.proto'
 
 class App extends Component {
+  componentDidMount(){
+    load(awesomProto, function(err, root) {
+      if (err)
+        throw err;
+    
+      // example code
+      const AwesomeMessage = root.lookupType("awesomepackage.AwesomeMessage");
+    
+      let message = AwesomeMessage.create({ awesomeField: "hello" });
+      console.log(`message = ${JSON.stringify(message)}`);
+    
+      let buffer = AwesomeMessage.encode(message).finish();
+      console.log(`buffer = ${Array.prototype.toString.call(buffer)}`);
+    
+      let decoded = AwesomeMessage.decode(buffer);
+      console.log(`decoded = ${JSON.stringify(decoded)}`);
+    });
+  }
   render() {
     return (
       <div className="App">
